369f3c251caacb4497740054c41b71d07e1f9ae4
rapportnetworks <33627036+rapportnetworks@users.noreply.github.com>
2018-04-05 16:24:59 +0100

    Initial commit

---
+++ devicetypes/rapportnetworks/room-name-virtual-device.src/room-name-virtual-device.groovy
@@ -0,0 +1,26 @@
+/**
+ *  Virtual Device Handler to Enable Room Name Capture
+ *
+ *  Copyright 2018 Alasdair Thin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Room Name Virtual Device", namespace: "rapportnetworks", author: "Alasdair Thin") {
+		capability "Bridge"
+	}
+
+	tiles {
+		multiAttributeTile(name:"name", type: "generic", width: 6, height: 4){
+		tileAttribute("device.name", key: "PRIMARY_CONTROL") {
+		attributeState("default", label: 'Room Name Virtual Device') } }
+	}
+}

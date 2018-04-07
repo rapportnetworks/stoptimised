f2767c1401b642383c8e3edd4863a6d22f02f098
altgithub <altgithub@users.noreply.github.com>
2018-04-07 13:37:46 +0100

    renamed

--- devicetypes/rapportnetworks/aeotec-hem-gen5-optimised.src/aeotec-hem-gen5-child-device.groovy
+++ devicetypes/rapportnetworks/aeotec-hem-gen5-optimised.src/aeotec-hem-gen5-child-device.groovy
@@ -14,7 +14,7 @@
  *
  */
 metadata {
-	definition (name: "Aeotec HEM Gen5 Optimised Child Device", namespace: "rapportnetworks", author: "Artur Draga") {
+	definition (name: "Aeotec HEM Gen5 Child Device", namespace: "rapportnetworks", author: "Artur Draga") {
 		capability "Energy Meter"
 		capability "Power Meter"
 		capability "Voltage Measurement"

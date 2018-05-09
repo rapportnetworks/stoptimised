2e02812358712962e346ad4a802335d4ee97872a
rapportnetworks <33627036+rapportnetworks@users.noreply.github.com>
2018-05-09 13:30:22 +0100

    Updated timestamp compensation for PIR reset Time

--- smartapps/rapportnetworks/influxdb-smart-logger.src/influxdb-smart-logger.groovy
+++ smartapps/rapportnetworks/influxdb-smart-logger.src/influxdb-smart-logger.groovy
@@ -333,14 +333,10 @@ def handleStateEvent(evt) {
     def pEvent = (eventTime > pEvents[1].date.time) ? pEvents[1] : pEvents[2]
     def pEventTime = pEvent.date.time
 
-    def offsetTime // offset to compensate for PIR reset Time
-    if (state.adjustInactiveTimestamp && evt.name == 'motion' && evt?.data) { // adjust timestamp of "inactive" status to compensate for PIRresetTime
-        def eventData = parseJson(evt.data)
-        if (eventData?.PIRresetTime) {
-            offsetTime = 1000 * eventData.PIRresetTime / 2
-            if (evt.value == 'inactive') eventTime -= offsetTime
-            if (pEvent.value == 'inactive') pEventTime -= offsetTime
-        }
+    def offsetTime = 1000 * 10 / 2
+    if (state.adjustInactiveTimestamp && evt.name == 'motion') { // adjust timestamp of "inactive" status to compensate for PIRresetTime
+        if (evt.value == 'inactive') eventTime -= offsetTime
+        if (pEvent.value == 'inactive') pEventTime -= offsetTime
     }
 
     def pTime = (eventTime - pEventTime)

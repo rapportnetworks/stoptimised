94073444df5a5a716d0bcaf85d569de6086c151b
rapportnetworks <33627036+rapportnetworks@users.noreply.github.com>
2018-12-05 18:02:12 +0000

    Updates

--- smartapps/rapportnetworks/configurator.src/configurator.groovy
+++ smartapps/rapportnetworks/configurator.src/configurator.groovy
@@ -11,7 +11,7 @@
  *
  *  Author: Alasdair Thin
  *
- *  Description: A SmartApp to log SmartThings device states to an InfluxDB database.
+ *  Description: A SmartApp to Send Configure Command to selected SmartThings Devices with the Configuration Capability.
  *
  *  Acknowledgements: Includes code originally developed by David Lomas (codersaur) and Kevin LaFramboise (krlaframboise).
  *
@@ -29,48 +29,56 @@
  *****************************************************************************************************************/
 
 definition (
-        name: "Configurator",
-        namespace: "rapportnetworks",
-        author: "Alasdair Thin",
-        description: "Configure SmartThings Devices",
-        category: "My Apps",
-        iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-        iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-        iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+        name: 'Configurator',
+        namespace: 'rapportnetworks',
+        author: 'Alasdair Thin',
+        description: 'Configure SmartThings Devices',
+        category: 'My Apps',
+        iconUrl: 'https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png',
+        iconX2Url: 'https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png',
+        iconX3Url: 'https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png'
 )
 
 preferences {
-    page(name: "mainPage")
-    page(name: "devicesPage")
+    page(name: 'mainPage')
+    page(name: 'devicesPage')
 }
 
 def mainPage() {
-    dynamicPage(name:"mainPage", uninstall:true, install:true) {
+    dynamicPage(name: 'mainPage', uninstall:true, install:true) {
 
-        section("General") {
+        section('Options') {
             input (
-                    name: "configLoggingLevelIDE",
-                    title: "IDE Live Logging Level. Messages with this level and higher will be logged to the IDE.",
-                    type: "enum",
-                    options: [ "0" : "None", "1" : "Error", "2" : "Warning", "3" : "Info", "4" : "Debug", "5" : "Trace" ],
-                    defaultValue: "3",
+                    name: 'configLoggingLevelIDE',
+                    title: 'IDE Live Logging Level. Messages with this level and higher will be logged to the IDE.',
+                    type: 'enum',
+                    options: [ '0': 'None', '1': 'Error', '2': 'Warning', '3': 'Info', '4': 'Debug', '5': 'Trace' ],
+                    defaultValue: '3',
                     displayDuringSetup: true,
-                    required: false,
+                    required: false
             )
             input(
                     name: 'clearSelections',
-                    title: 'Clear device selections after sending configure command.',
+                    title: 'Clear device selections after sending Configure command.',
                     type: 'bool',
                     defaultValue: true,
                     displayDuringSetup: true,
-                    required: false,
+                    required: false
+            )
+            input(
+                    name: 'enableTouch',
+                    title: 'Enable App Touch to send Configure command.',
+                    type: 'bool',
+                    defaultValue: false,
+                    displayDuringSetup: true,
+                    required: false
             )
         }
 
 
         if (state.devicesConfigured && !settings.clearSelections) {
-            section("Selected Devices") {
-                getPageLink("devicesPageLink", "Tap to change", "devicesPage", null, buildSummary(selectedDeviceNames))
+            section('Selected Devices') {
+                getPageLink('devicesPageLink', 'Tap to change', 'devicesPage', null, buildSummary(selectedDeviceNames))
             }
         }
         else {
@@ -80,28 +88,28 @@ def mainPage() {
 }
 
 def devicesPage() {
-    dynamicPage(name:"devicesPage") {
+    dynamicPage(name: 'devicesPage') {
         devicesPageContent
     }
 }
 
 private getDevicesPageContent() {
-    section("Choose Devices") {
-        paragraph 'Each device only needs to be selected once and which field you select it from has no effect on which events will be logged for it.'
+    section('Choose Devices') {
+        paragraph 'Select devices to send Configure command to. Only devices with Configuration capability will receive the command.'
 
         capabilities.each {
-            if (it.cap != 'bridge') {
-                try {
-                    input "${it.cap}Pref", "capability.${it.cap}",
-                            title: "${it.title}:",
-                            multiple: true,
-                            hideWhenEmpty: true,
-                            required: false,
-                            submitOnChange: true
-                }
-                catch (final e) {
-                    logTrace "Failed to create input for ${it}: ${e.message}"
-                }
+            try {
+                input(
+                        "${it.cap}Pref",
+                        "capability.${it.cap}",
+                        title: "${it.title}:",
+                        multiple: true,
+                        hideWhenEmpty: true,
+                        required: false,
+                        submitOnChange: true)
+            }
+            catch (e) {
+                logger("Preferences: Failed to create input for ${it}: ${e.message}", 'trace')
             }
         }
 
@@ -110,31 +118,33 @@ private getDevicesPageContent() {
 
 private getSelectedDeviceNames() {
     try {
-        return selectedDevices?.collect { "${it?.displayName}" + "${(it.hasCapability('Configuration')) ? '+' : '-'}" + "${(it.hasCommand('configure')) ? '+' : '-'}" }?.sort() // check this works first then add ${(it.hasCapability('Configuration')) ? '+' : '-'}
+        selectedDevices?.collect {
+            "${it?.displayName}${(it.hasCapability('Configuration')) ? '+' : '-'}${(it.hasCommand('configure')) ? '+' : '-'}"
+        }?.sort()
     }
-    catch (final e) {
-        logWarn "Error while getting selected device names: ${e.message}"
-        return []
+    catch (e) {
+        logger("Preferences: Error while getting selected device names: ${e.message}", 'warn')
+        []
     }
 }
 
 private getSelectedDevices() {
-    final def devices = []
+    def devices = []
     capabilities?.each {
         try {
             if (settings?."${it.cap}Pref") {
                 devices << settings?."${it.cap}Pref"
             }
         }
-        catch (final e) {
-            logWarn "Error while getting selected devices for capability ${it}: ${e.message}"
+        catch (e) {
+            logger("Preferences: Error while getting selected devices for capability ${it}: ${e.message}", 'warn')
         }
     }
-    return devices?.flatten()?.unique { it.id }
+    devices?.flatten()?.unique { it.id }
 }
 
 private getPageLink(final linkName, final linkText, final pageName, final args=null, final desc="", final image=null) {
-    final def map = [
+    def map = [
             name: "$linkName",
             title: "$linkText",
             description: "$desc",
@@ -160,60 +170,57 @@ private static buildSummary(final items) {
  *****************************************************************************************************************/
 def installed() { // runs when the app is first installed
     state.loggingLevelIDE = 5
-    log.debug "${app.label}: Installed with settings: ${settings}"
+    logger("Installed App: ${app.label}: Installed with settings: ${settings}", 'trace')
 }
 
 def uninstalled() { // runs when the app is uninstalled
-    logger("uninstalled()","trace")
+    logger("Uninstalled App: ${app.label}.",'trace')
 }
 
 def updated() { // runs when app settings are changed
-    logger("updated()","trace")
+    if (settings.enableTouch) {
+        subscribe(app, handleAppTouch)
+    }
+    else {
+        unsubscribe()
+    }
+
+    logger("Updated App: ${app.label}.", 'trace')
 
     state.loggingLevelIDE = (settings.configLoggingLevelIDE) ? settings.configLoggingLevelIDE.toInteger() : 3
 
-    if (selectedDevices) {
-        state.devicesConfigured = true
-    }
-    else {
-        logger('Unconfigured - Choose Devices', 'debug')
-    }
+    (selectedDevices) ? state.devicesConfigured = true : logger('Unconfigured - Choose Devices', 'debug')
 
     if (state.devicesConfigured) configureCommand()
 
-    if (settings.clearSelections) {
-        runIn(30, resetPrefs)
-        state.devicesConfigured = false
-    }
+    if (settings.clearSelections) runIn(15, resetPrefs)
 }
 
 /*****************************************************************************************************************
  *  Event Handlers:
  *****************************************************************************************************************/
-def handleAppTouch(final evt) { // handleAppTouch(evt) - used for testing
-    logger("handleAppTouch()","trace")
+def handleAppTouch(evt) { // SmartApp Touch event
+    logger('handleAppTouch: event triggered','trace')
+    if (state.devicesConfigured) configureCommand()
 }
 
 /*****************************************************************************************************************
  *  Main Commands:
  *****************************************************************************************************************/
 def configureCommand() {
-    selectedDevices?.each  { final dev ->
-        if (!dev.displayName.startsWith("~")) {
-            if (dev.hasCapability("Configuration")) {
-                log.debug "${dev.displayName} has Configuration Capability"
-                if (dev.hasCommand("configure")) {
-                    log.debug "${dev.displayName} has configure Command"
-                    dev.configure()
-                    log.debug "configure Command sent to ${dev.displayName}"
-                }
-                else {
-                    log.debug "${dev.displayName} does not have configure Command"
-                }
+    selectedDevices?.each  {
+        if (it.hasCapability('Configuration')) {
+            logger("${it.displayName} has Configuration Capability.", 'debug')
+            if (it.hasCommand('configure')) {
+                logger("Configure Command sent to ${it.displayName}.", 'info')
+                it.configure()
             }
             else {
-                log.debug "${dev.displayName} does not have Configuration Capability"
+                logger("${it.displayName} does not have Configure Command.", 'info')
             }
+        }
+        else {
+            logger("${it.displayName} does not have Configuration Capability.", 'debug')
         }
     }
 }
@@ -223,30 +230,30 @@ def configureCommand() {
  *  Private Helper Functions:
  *****************************************************************************************************************/
 def resetPrefs() {
-    // app.updateSetting(inputName, [type: type, value: value])
+    state.devicesConfigured = false
     capabilities?.each {
         if (settings?."${it.cap}Pref") {
+            logger('resetPrefs: Resetting preferences.', 'info')
             app.updateSetting("${it.cap}Pref", [])
-            logger('Resetting preferences.', 'info')
         }
     }
 }
 
-private logger(final msg, final level = "debug") { // Wrapper function for all logging
+private logger(final msg, final level = 'debug') {
     switch(level) {
-        case "error":
+        case 'error':
             if (state.loggingLevelIDE >= 1) log.error msg
             break
-        case "warn":
+        case 'warn':
             if (state.loggingLevelIDE >= 2) log.warn msg
             break
-        case "info":
+        case 'info':
             if (state.loggingLevelIDE >= 3) log.info msg
             break
-        case "debug":
+        case 'debug':
             if (state.loggingLevelIDE >= 4) log.debug msg
             break
-        case "trace":
+        case 'trace':
             if (state.loggingLevelIDE >= 5) log.trace msg
             break
         default:
@@ -257,52 +264,5 @@ private logger(final msg, final level = "debug") { // Wrapper function for all l
 
 private static getCapabilities() { [
         [title: 'Actuators', cap: 'actuator'],
-        [title: 'Bridges', cap: 'bridge'],
         [title: 'Sensors', cap: 'sensor', attr: ['buttonClicks', 'current', 'pressure', 'reactiveEnergy', 'reactivePower', 'totalEnergy']],
-        [title: 'Acceleration Sensors', cap: 'accelerationSensor', attr: 'acceleration'],
-        [title: 'Alarms', cap: 'alarm', attr: 'alarm'],
-        [title: 'Batteries', cap: 'battery', attr: 'battery'],
-        [title: 'Beacons', cap: 'beacon', attr: 'presence'],
-        [title: 'Bulbs', cap: 'bulb', attr: 'switch'],
-        [title: 'Buttons', cap: 'button', attr: ['button', 'numberOfButtons']],
-        [title: 'Carbon Dioxide Measurement Sensors', cap: 'carbonDioxideMeasurement', attr: 'carbonDioxide'],
-        [title: 'Carbon Monoxide Detectors', cap: 'carbonMonoxideDetector', attr: 'carbonMonoxide'],
-        [title: 'Color Control Devices', cap: 'colorControl', attr: ['color', 'hue', 'saturation']],
-        [title: 'Color Temperature Devices', cap: 'colorTemperature', attr: 'colorTemperature'],
-        [title: 'Consumable Devices', cap: 'consumable', attr: 'consumableStatus'],
-        [title: 'Contact Sensors', cap: 'contactSensor', attr: 'contact'],
-        [title: 'Doors', cap: 'doorControl', attr: 'door'],
-        [title: 'Energy Meters', cap: 'energyMeter', attr: 'energy'],
-        [title: 'Garage Doors', cap: 'garageDoorControl', attr: 'door'],
-        [title: 'Holdable Buttons', cap: 'holdableButton', attr: ['button', 'numberOfButtons']],
-        [title: 'Illuminance Measurement Sensors', cap: 'illuminanceMeasurement', attr: 'illuminance'],
-        [title: 'Lights', cap: 'light', attr: 'switch'],
-        [title: 'Locks', cap: 'lock', attr: 'lock'],
-        [title: 'Motion Sensors', cap: 'motionSensor', attr: 'motion'],
-        [title: 'Music Players', cap: 'musicPlayer', attr: ['level', 'mute', 'status', 'trackDescription']],
-        [title: 'Outlets', cap: 'outlet', attr: 'switch'],
-        [title: 'Power Meters', cap: 'powerMeter', attr: 'power'],
-        [title: 'Power Sources', cap: 'powerSource', attr: 'powerSource'],
-        [title: 'Power', cap: 'power', attr: 'powerSource'],
-        [title: 'Presence Sensors', cap: 'presenceSensor', attr: 'presence'],
-        [title: 'Relative Humidity Measurement Sensors', cap: 'relativeHumidityMeasurement', attr: 'humidity'],
-        [title: 'Relay Switches', cap: 'relaySwitch', attr: 'switch'],
-        [title: 'Shock Sensors', cap: 'shockSensor', attr: 'shock'],
-        [title: 'Signal Strength Sensors', cap: 'signalStrength', attr: ['lqi', 'rssi']],
-        [title: 'Sleep Sensors', cap: 'sleepSensor', attr: 'sleeping'],
-        [title: 'Smoke Detectors', cap: 'smokeDetector', attr: 'smoke'],
-        [title: 'Sound Pressure Level Sensors', cap: 'soundPressureLevel', attr: 'soundPressureLevel'],
-        [title: 'Sound Sensors', cap: 'soundSensor', attr: 'sound'],
-        [title: 'Switch Level Sensors', cap: 'switchLevel', attr: 'level'],
-        [title: 'Switches', cap: 'switch', attr: 'switch'],
-        [title: 'Tamper Alert Sensors', cap: 'tamperAlert', attr: 'tamper'],
-        [title: 'Temperature Measurement Sensors', cap: 'temperatureMeasurement', attr: 'temperature'],
-        [title: 'Thermostats', cap: 'thermostat', attr: ['heatingSetpoint', 'temperature', 'thermostatFanMode', 'thermostatMode', 'thermostatOperatingState', 'thermostatSetpoint']],
-        [title: 'Three Axis Sensors', cap: 'threeAxis', attr: 'threeAxis'],
-        [title: 'Touch Sensors', cap: 'touchSensor', attr: 'touch'],
-        [title: 'Ultraviolet Index Sensors', cap: 'ultravioletIndex', attr: 'ultravioletIndex'],
-        [title: 'Valves', cap: 'valve', attr: 'valve'],
-        [title: 'Voltage Measurement Sensors', cap: 'voltageMeasurement', attr: 'voltage'],
-        [title: 'Water Sensors', cap: 'waterSensor', attr: 'water'],
-        [title: 'Window Shades', cap: 'windowShade', attr: 'windowShade'],
 ] }
